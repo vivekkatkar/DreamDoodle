d83e3983f0431f077f40e2a3483729f4ea85dae9
unknown <manasgedam>
2024-01-03 22:26:48 +0530

    Update SideNav and add journal layout and page

--- app/(routes)/documents/_components/SideNav.jsx
+++ app/(routes)/documents/_components/SideNav.jsx
@@ -1,16 +1,15 @@
-import { Search, Settings } from "lucide-react";
+"use client"
 
+import Link from "next/link";
 const SideNavigation = () => {
     return (
         <>
-            <aside
-            className="group/sidebar h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-[9999]"
-            >
-                <div>
-                    Navigation
-                    links
-                </div>
-            </aside>
+            <section className="flex gap-8 justify-center mt-32">
+             <Link href="documents/journal"><div className="w-36 h-14 bg-black text-white rounded-xl text-center flex items-center justify-center cursor-pointer">Write Journal</div></Link>
+                <div className="w-36 h-14 bg-black text-white rounded-xl text-center flex items-center justify-center cursor-pointer">Add Tasks </div>
+                <div className="w-36 h-14 bg-black text-white rounded-xl text-center flex items-center justify-center cursor-pointer">Study Time</div>
+            </section>
+            {/* heatmap */}
         </>
     );
 }
---
+++ app/(routes)/documents/journal/page.jsx
@@ -0,0 +1,7 @@
+const Journal = () => {
+    return ( <>
+        Journal
+    </> );
+}
+ 
+export default Journal;
